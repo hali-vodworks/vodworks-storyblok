8f866b76429fe1789fedddd5588cc883adb2be66
snawaz_vodworks <snawaz@vodworks.com>
2025-06-03 12:24:55 +0500

    minor css update

--- components/Sections/CsReview.vue
+++ components/Sections/CsReview.vue
@@ -4,7 +4,7 @@
             <div class="review relative md:max-w-4/5 mx-auto">
                 <img class="quote-img" :src="data.icon.filename" :alt="data.icon.alt" />
                 <div class="content">
-                    <h4 class="color-white leading-8">{{ data.review_text }}</h4>
+                    <h4 class="color-white leading-8" style="text-transform: initial;">{{ data.review_text }}</h4>
 
                     <div class="flex items-center gap-6 mt-8">
                         <img v-if="data.reviewer_image?.filename" class="reviewer_image"
