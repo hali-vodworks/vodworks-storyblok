cbf9c750fdf60878e3f158156730e84c388db3d5
snawaz_vodworks <snawaz@vodworks.com>
2025-07-21 12:09:48 +0500

    Added Eloq case study

---
+++ pages/cases/eloqi.vue
@@ -0,0 +1,279 @@
+<!-- eslint-disable vue/no-v-html -->
+<template>
+  <div>
+    <!-----------  Hero section -------------------------------------------------------->
+    <CsHero :data="{
+      content: getSingleCsHero,
+      hasMorePadding: true,
+      imageHeightFull: false,
+    }" />
+    <!---------------------------------------------------------------------------------->
+
+    <!-------------- Brief -------------------------------------------------------------->
+    <CsBrief :data="Brief" />
+    <!----------------------------------------------------------------------------------->
+
+    <!--------------- The Scope  -------------------------------------------------------->
+    <CsScope :data="Scope" />
+    <!----------------------------------------------------------------------------------->
+
+    <!-------------- Featured Image 1---------------------------------------------------->
+    <CsFeaturedImage :data="getSingleCsFeaturedImage1" />
+    <!----------------------------------------------------------------------------------->
+
+    <!--------------- The clients VidScape Helped  -------------------------------------->
+    <section v-if="Approach" class="single-cs lg:py-32 py-14 bgColor-normal-grey">
+      <div class="mx-auto container">
+        <div class="">
+          <div class="text-center md:max-w-4/5 mx-auto">
+            <h2 v-in-viewport.once><span class="bgFill"><span class="textClip">{{ Approach.title }}</span></span>
+            </h2>
+            <div class="mt-8 lg:mt-16 text-left" v-html="$md.render(Approach.description)"> </div>
+
+          </div>
+
+          <!-- CARDs -->
+          <div class="grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 mx-auto gap-8 mt-8 lg:mt-16">
+            <template v-for="(card, index) in Approach.cards">
+              <div :key="index" class="default-card card-utilities text-left">
+                <h4> {{ card.title }}</h4>
+                <p class="flex-grow-1" v-html="$md.render(card.description)"></p>
+              </div>
+            </template>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <!----------------------------------------------------------------------------------->
+
+    <!---------------  Technical Stack -------------------------------------------------->
+    <CsTechStack :data="{
+      content: TechnicalStack,
+      layout: 'cols-5',
+    }" />
+    <!----------------------------------------------------------------------------------->
+
+    <!----------------------------------Team--------------------------------------------->
+    <CsTeam :data="{
+      content: Team,
+      layout: 'md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4'
+    }" />
+    <!----------------------------------------------------------------------------------->
+
+    <!--------------- The Result -------------------------------------------------------->
+    <CsResult :data="Result" />
+    <!----------------------------------------------------------------------------------->
+
+    <!---------------------------FAQs----------------------------------------------------->
+    <FAQs :payload="FAQs" />
+    <!------------------------------------------------------------------------------------>
+
+    <!------------------------------- Get in Touch with us-------------------------------->
+    <GetInTouchWithUs :data="{
+      title: 'Get in Touch with us',
+      isDarkSectionAtTop: true
+    }" />
+    <!------------------------------------------------------------------------------------->
+
+  </div>
+</template>
+
+<script>
+
+const loadData = function ({
+  api,
+  cacheVersion,
+  errorCallback,
+  version,
+  path,
+}) {
+  return api
+    .get(`cdn/stories${path}`, {
+      version,
+      resolve_links: 'story,url',
+      resolve_relations: 'case-studies-container.case_studies,case_studies.case-study,case-studies-container.case-study,faqs-container.list_of_faqs',
+      cv: cacheVersion,
+    })
+    .then((res) => {
+      return res.data
+    })
+    .catch((res) => {
+      if (!res.response) {
+        errorCallback({
+          statusCode: 404,
+          message: 'Failed to receive content form api',
+        })
+      } else {
+        errorCallback({
+          statusCode: res.response.status,
+          message: res.response.data,
+        })
+      }
+    })
+}
+
+export default {
+
+  asyncData(context) {
+    // Check if we are in the editing mode
+    let editMode = true
+    if (
+      context.query._storyblok ||
+      context.isDev ||
+      (typeof window !== 'undefined' &&
+        window.localStorage.getItem('_storyblok_draft_mode'))
+    ) {
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem('_storyblok_draft_mode', '1')
+        if (window.location === window.parent.location) {
+          window.localStorage.removeItem('_storyblok_draft_mode')
+        }
+      }
+      editMode = true
+    }
+    const version = editMode ? 'draft' : 'published'
+    const path = context.route.path === '/' ? '/home' : context.route.path
+    // Load the JSON from the API
+    return loadData({
+      version,
+      api: context.app.$storyapi,
+      errorCallback: context.error,
+      path,
+    })
+  },
+
+
+  head() {
+    return {
+      title: `${this.story.content.title}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: `${this.story.content.description}`,
+        },
+        {
+          hid: 'og-type',
+          property: 'og:type',
+          content: 'website'
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: `${this.story.content.title}`,
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          property: 'og:description',
+          content: `${this.story.content.description}`,
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: `${this.story.content.thumbnail.filename}`,
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: `/${this.story.full_slug}`,
+        },
+        // For Twitter
+        {
+          hid: 't-type',
+          name: 'twitter:card',
+          content: `${this.story.content.thumbnail.filename}`,
+        },
+      ],
+      script: [
+        {
+          type: 'application/ld+json',
+          json: this.generateFaqSchema(),
+        },
+      ],
+    }
+  },
+
+
+  computed: {
+    getSingleCsHero() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'single-cs-hero';
+      })
+    },
+    Brief() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'cs-fw-brief';
+      })
+    },
+    Scope() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'cs-scope';
+      })
+    },
+    getSingleCsFeaturedImage1() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'single-cs-featured-image-1';
+      })
+    },
+    Approach() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'cs-approach_with_cards';
+      })
+    },
+    TechnicalStack() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'cs-technical-stack';
+      })
+    },
+    Team() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'cs_team';
+      })
+    },
+    Result() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'cs-result';
+      })
+    },
+    FAQs() {
+      return this.story.content.cs_full_details.find(function (obj) {
+        return obj.component === 'faqs-container';
+      })
+    },
+
+  },
+
+  mounted() {
+    this.$storybridge.on(['input', 'published', 'change'], (event) => {
+      if (event.action === 'input') {
+        if (event.story.id === this.story.id) {
+          this.story.content = event.story.content
+        }
+      } else if (!event.slugChanged) {
+        window.location.reload()
+      }
+    })
+  },
+
+  methods: {
+    generateFaqSchema() {
+      return {
+        "@context": "https://schema.org",
+        "@type": "FAQPage",
+        "mainEntity": this.FAQs.list_of_faqs.map(faq => ({
+          "@type": "Question",
+          "name": faq.content.question,
+          "acceptedAnswer": {
+            "@type": "Answer",
+            "text": faq.content.answer
+          }
+        }))
+      };
+    }
+  }
+
+}
+
+</script>
